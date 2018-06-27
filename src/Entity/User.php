3c1e260c76746f76d55969d11358588fe70f7720
Unknown <arthur.simonian@outlook.fr>
2018-06-27 14:37:10 +0200

    debut d'utilisation utilisateur

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,11 +3,12 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User implements UserInterface, \Serializable
 {
     /**
      * @ORM\Id()
@@ -15,7 +16,12 @@ class User
      * @ORM\Column(type="integer")
      */
     private $id;
-
+    
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+    
     /**
      * @ORM\Column(type="string", length=255)
      */
@@ -25,12 +31,29 @@ class User
      * @ORM\Column(type="string", length=255)
      */
     private $password;
-
+    
+    /**
+     * @var array
+     *
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+    
     public function getId()
     {
         return $this->id;
     }
 
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+ 
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+    
     public function getMail(): ?string
     {
         return $this->mail;
@@ -54,4 +77,67 @@ class User
 
         return $this;
     }
+    
+    /**
+     * Retourne les rôles de l'user
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+ 
+        // Afin d'être sûr qu'un user a toujours au moins 1 rôle
+        if (empty($roles)) {
+            $roles[] = 'ROLE_USER';
+        }
+ 
+        return array_unique($roles);
+    }
+ 
+    public function setRoles(array $roles): void
+    {
+        $this->roles = $roles;
+    }
+ 
+    /**
+     * Retour le salt qui a servi à coder le mot de passe
+     *
+     * {@inheritdoc}
+     */
+    public function getSalt(): ?string
+    {
+        // See "Do you need to use a Salt?" at https://symfony.com/doc/current/cookbook/security/entity_provider.html
+        // we're using bcrypt in security.yml to encode the password, so
+        // the salt value is built-in and you don't have to generate one
+ 
+        return null;
+    }
+ 
+    /**
+     * Removes sensitive data from the user.
+     *
+     * {@inheritdoc}
+     */
+    public function eraseCredentials(): void
+    {
+        // Nous n'avons pas besoin de cette methode car nous n'utilions pas de plainPassword
+        // Mais elle est obligatoire car comprise dans l'interface UserInterface
+        // $this->plainPassword = null;
+    }
+ 
+    /**
+     * {@inheritdoc}
+     */
+    public function serialize(): string
+    {
+        return serialize([$this->id, $this->username, $this->password]);
+    }
+ 
+    /**
+     * {@inheritdoc}
+     */
+    public function unserialize($serialized): void
+    {
+        [$this->id, $this->username, $this->password] = unserialize($serialized, ['allowed_classes' => false]);
+    }
+    
 }
